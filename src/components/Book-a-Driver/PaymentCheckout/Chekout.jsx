dc24d240646d72e6ab17f3f97c56c6a738937c24
Kaushik N <kaushik1603n@gmail.com>
2024-03-22 17:09:01 +0530

    all page

--- src/components/Book-a-Driver/PaymentCheckout/Chekout.jsx
+++ src/components/Book-a-Driver/PaymentCheckout/Chekout.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Chekout.css";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import {
   faUser,
   faCreditCard,
@@ -11,6 +13,122 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 function Chekout() {
+  const navigate = useNavigate();
+  const { amount } = useParams();
+  const driverId = localStorage.getItem("driverid");
+
+
+  const [userName, setUserName] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [error, setError] = useState("");
+
+  const passwordLength = 15;
+  const incluseLowercase = true;
+  const incluseUppercase = true;
+  const incluseNumbers = true;
+
+  const handleInputChange = (e, type) => {
+    switch (type) {
+      case "userName":
+        setError("");
+        setUserName(e.target.value);
+        if (e.target.value === "") {
+          setError("Username has left blank!");
+        }
+        break;
+      case "cardNumber":
+        setError("");
+        setCardNumber(e.target.value);
+        if (e.target.value === "") {
+          setError("Card Number has left blank!");
+        }
+        break;
+      case "expiryDate":
+        setError("");
+        setExpiryDate(e.target.value);
+        if (e.target.value === "") {
+          setError("Expiry Date has left blank!");
+        }
+        break;
+      case "cvv":
+        setError("");
+        setCvv(e.target.value);
+        if (e.target.value === "") {
+          setError("CVV has left blank!");
+        }
+        break;
+      default:
+    }
+  };
+
+  const DriverVerifyInfo = async (driver) => {
+    const formData = new FormData();
+    formData.append("userName", userName);
+    formData.append("driverid", driverId);
+    formData.append("cardNumber", cardNumber);
+    formData.append("expiryDate", expiryDate);
+    formData.append("cvv", cvv);
+    formData.append("transactionId", Trid);
+    formData.append("amount", amount);
+
+    const response = await axios.post(
+      "http://localhost/devtest/reactjs/payment/payment.php",
+      formData,
+      {
+        headers: { "Content-Type": "multipart/form-data" },
+      }
+    );
+    console.log(response.data);
+
+    if (response.data.success) {
+    }
+  };
+  
+  const generateRandomNumber = (
+    length,
+    incluseLowercase,
+    incluseUppercase,
+    incluseNumbers
+  ) => {
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+
+    let allowedChars = "";
+    let tranId = "";
+
+    allowedChars += incluseLowercase ? lowercaseChars : "";
+    allowedChars += incluseUppercase ? uppercaseChars : "";
+    allowedChars += incluseNumbers ? numberChars : "";
+
+    if (length <= 0) {
+      return ` (password lenngth must be at least 1)`;
+    }
+
+    if (allowedChars.length === 0) {
+      return `(At least 1 charecter need to be selected)`;
+    }
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * allowedChars.length);
+      tranId += allowedChars[randomIndex];
+    }
+
+    return tranId;
+  };
+  const Trid = generateRandomNumber(
+    passwordLength,
+    incluseLowercase,
+    incluseUppercase,
+    incluseNumbers
+    );
+    
+    const handleSubmit = async () => {
+      DriverVerifyInfo();
+      navigate(`/payment-section`); 
+    };
   return (
     <div>
       <div className="modal">
@@ -22,7 +140,9 @@ function Chekout() {
             <input
               type="text"
               id="userName"
-              name="UserName"
+              name="userName"
+              value={userName}
+              onChange={(e) => handleInputChange(e, "userName")}
               placeholder="Enter Your Name"
               required
             />
@@ -32,6 +152,8 @@ function Chekout() {
               id="cardNumber"
               name="cardNumber"
               placeholder="Card Number"
+              value={cardNumber}
+              onChange={(e) => handleInputChange(e, "cardNumber")}
               required
             />
             <div className="date-cv">
@@ -41,6 +163,8 @@ function Chekout() {
                 id="expiryDate"
                 name="expiryDate"
                 placeholder="Expiry Date"
+                value={expiryDate}
+                onChange={(e) => handleInputChange(e, "expiryDate")}
                 required
               />
               <FontAwesomeIcon icon={faLock} className="icon" />
@@ -49,6 +173,8 @@ function Chekout() {
                 id="cvv"
                 name="cvv"
                 placeholder="CVV"
+                value={cvv}
+                onChange={(e) => handleInputChange(e, "cvv")}
                 required
               />
             </div>
@@ -57,12 +183,16 @@ function Chekout() {
               type="text"
               id="amount"
               name="amount"
+              value={amount}
+              onChange={(e) => handleInputChange(e, "amount")}
               placeholder="Amount"
               readOnly
             />
-            <Link to="/payment-section">
-              <button type="submit">Pay Now</button>
-            </Link>
+            {/* <Link to="/payment-section"> */}
+            <button type="submit" onClick={() => handleSubmit()}>
+              Pay Now
+            </button>
+            {/* </Link> */}
           </form>
         </div>
       </div>
